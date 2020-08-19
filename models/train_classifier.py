9399d837fe731cb4ad5d534c0ee2ab6dd3a5516b
shitianyi2020 <shitianyi2016@163.com>
2020-08-19 14:31:30 +0000

    update on 19th Aug.

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -70,7 +70,7 @@ def evaluate_model(model, X_test, Y_test, category_names):
 
 
 def save_model(model, model_filepath):
-    pickle.dump(model,model_filepath)
+    pickle.dump(model.best_estimator_, open(model_filepath, 'wb'))
 
 def main():
     if len(sys.argv) == 3:
