2152a870bc726bf3be0448e0a8670daa58e2c5df
shitianyi2020 <shitianyi2016@163.com>
2020-08-20 07:46:37 +0000

    update on 20th Aug.

--- data/process_data.py
+++ data/process_data.py
@@ -4,27 +4,67 @@ import numpy as np
 from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
+    
+    """
+    Args:
+        messages_filepath: The file path of messages.csv
+        categories_filepath: The file path of categories.cv
+
+    Returns:
+        df: Merged dataset on ID
+    """
     messages = pd.read_csv(messages_filepath)
     categories = pd.read_csv(categories_filepath)
     df = pd.merge(messages,categories,on='id')
     return df
 
 def clean_data(df):
+    
+    """
+    Args:
+        df: Combined dataset
+        
+    Returns:
+        df: Cleaned dataset with split categories
+    """
+    # create a dataframe of the 36 individual category columns
     categories = df['categories'].str.split(';',expand=True)
+    
+    # select the first row of the categories dataframe
     row = categories.loc[0,:]
+    
+    # extract a list of new column names for categories.
     category_colnames = list(x[:-2] for x in row)
+    
+    # rename the columns of categories
     categories.columns = category_colnames
+    
+    # convert category values to just numbers 0 or 1
     for column in categories:
         categories[column] = categories[column].astype(str).apply(lambda x: x.split('-')[-1])
         categories[column] = categories[column].astype('int64')
+    
+    # replace categories column in df with new category columns.
     df.drop('categories',axis=1,inplace=True)
     df = pd.concat([df,categories],axis=1)
-    df.related[df['related'] == 2] = 1
+    
+    # replace the related value 2 with 1
+    df.related.replace(2,1,inplace=True)
+    
+    # # drop duplicates
     df = df.drop_duplicates()
     return df
     
 def save_data(df, database_filename):
-    engine = create_engine('sqlite:///data/DisasterResponse.db')
+    
+    """
+    Args:
+        df: The cleaned dataset
+        database_filename : The file path to save the db
+    Returns:
+        None
+    """
+    engine = create_engine('sqlite:///' + database_filename)
     df.to_sql('df', engine, index=False)  
 
 def main():
